4d090fab0a75d2d9c8d23d0a530d6b57499a5f9d
Zeljko <zeljko.marenovic@quantox.com>
2018-07-24 12:16:15 +0200

    Transformers

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -7,8 +7,16 @@ use App\Http\Resources\UserResource;
 use App\User;
 use Illuminate\Http\Request;
 
+/**
+ * Class HomeController
+ * @package App\Http\Controllers\Api
+ */
 class HomeController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function index(Request $request)
     {
         $users = User::where('id', '!=', $request->user()->id)->get();
